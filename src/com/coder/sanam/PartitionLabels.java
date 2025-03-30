8a2822fa2d059b78de07debe1f0aeb9a07711a1f
Shubham Rathod <shubhamksrathod26@gmail.com>
2025-03-30 10:24:41 +0530

    leetcode POTD 30/03 added optimal approach

--- src/com/coder/sanam/PartitionLabels.java
+++ src/com/coder/sanam/PartitionLabels.java
@@ -1,15 +1,32 @@
 package com.coder.sanam;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class PartitionLabels {
 
     public static void main(String[] args) {
         PartitionLabels partitionLabels = new PartitionLabels();
         System.out.println(partitionLabels.partitionLabels("ababcbacadefegdehijhklij"));
+    }
+
+    public List<Integer> partitionLabelsOptimal(String s) {
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence.put(s.charAt(i), i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastOccurrence.get(s.charAt(i)));
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
     }
 
     public List<Integer> partitionLabels(String s) {
