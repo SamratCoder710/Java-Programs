8201cc36cd918ee2a69b23309a4c4160e2f0ac74
Shubham Rathod <shubhamksrathod26@gmail.com>
2026-08-01 17:29:06 +0530

    interview coding questions easy (not so easy if you don't know the syntax without autocomplete)

---
+++ src/com/coder/sanam/interviewPrep/HashTag.java
@@ -0,0 +1,20 @@
+package com.coder.sanam.interviewPrep;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class HashTag {
+
+    static void main() {
+        String sentence = "hello world java";
+
+        String k =
+                Arrays.stream(sentence.split(" "))
+                        .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1))
+                        .collect(Collectors.joining(""));
+
+        // String.join(delimited,Iterable charSequence)
+        String hashTag = "#" + k;
+        System.out.println(hashTag);
+    }
+}
